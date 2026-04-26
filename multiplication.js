4f7e262ccdf97296198f767fa03d7a9049a41b26
lopsig <jonathan.ls.trabajo@gmail.com>
2026-04-26 12:32:35 -0500

    update createTable function for Table 3 and Table 5

--- multiplication.js
+++ multiplication.js
@@ -1,6 +1,43 @@
 const createTable = () => {
-  let table = document.getElementById("table")
-  table.innerHTML = "<h1>PROBANDO</h1>";
+  const table = document.getElementById("table");
+  table.innerHTML = ""
+  
+  for (let i = 1; i <= 10; i++){
+    const file_i = document.createElement("div")
+    file_i.className = "fila";
+    file_i.style = "--delay:0.10s"
+    const spanOperation_i = document.createElement("span")
+    spanOperation_i.className = "operacion";
+    const spanFactor1_i = document.createElement("span")
+    spanFactor1_i.className = "num-azul";
+    spanFactor1_i.textContent = 5
+    const spanSimbol_i = document.createElement("span")
+    spanSimbol_i.textContent = "  X  "
+    const spanFactor2_i = document.createElement("span")
+    spanFactor2_i.className = "num-plomo";
+    spanFactor2_i.textContent = i
+    const spanSimbolResult_i = document.createElement("span")
+    spanSimbolResult_i.className = "signo";
+    spanSimbolResult_i.textContent = "=";
+    const spanResult_i = document.createElement("span")
+    spanResult_i.className = "resultado";
+    spanResult_i.textContent = 5 * i
+  
+  
+    spanOperation_i.appendChild(spanFactor1_i)
+    spanOperation_i.appendChild(spanSimbol_i)
+    spanOperation_i.appendChild(spanFactor2_i)
+  
+  
+    file_i.appendChild(spanOperation_i)
+    file_i.appendChild(spanSimbolResult_i)
+    file_i.appendChild(spanResult_i)
+    table.appendChild(file_i)
 
-  console.log("PROBANDO")
-}
+    
+  }
+
+  
+
+
+};
