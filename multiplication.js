bfc2a8fd6adfb5e8fdea19d9ab4cb90be112b596
lopsig <jonathan.ls.trabajo@gmail.com>
2026-04-26 13:27:02 -0500

    added input

--- multiplication.js
+++ multiplication.js
@@ -1,43 +1,46 @@
 const createTable = () => {
   const table = document.getElementById("table");
-  table.innerHTML = ""
+  table.innerHTML = "";
+  const number = document.getElementById("txtInput");
+  const numberValue = number.value;
+
+  const inputFactor = document.getElementById("inputFactor");
+  inputFactor.innerHTML = numberValue
   
-  for (let i = 1; i <= 10; i++){
-    const file_i = document.createElement("div")
+  const factor = document.getElementById("factor");
+  factor.innerHTML = "x" + numberValue
+
+  const maxValue = document.getElementById("maxValue");
+  maxValue.innerHTML = numberValue * 10
+
+  for (let i = 1; i <= 10; i++) {
+    const file_i = document.createElement("div");
     file_i.className = "fila";
-    file_i.style = "--delay:0.10s"
-    const spanOperation_i = document.createElement("span")
+    file_i.style = "--delay:0.10s";
+    const spanOperation_i = document.createElement("span");
     spanOperation_i.className = "operacion";
-    const spanFactor1_i = document.createElement("span")
+    const spanFactor1_i = document.createElement("span");
     spanFactor1_i.className = "num-azul";
-    spanFactor1_i.textContent = 5
-    const spanSimbol_i = document.createElement("span")
-    spanSimbol_i.textContent = "  X  "
-    const spanFactor2_i = document.createElement("span")
+    spanFactor1_i.textContent = numberValue;
+    const spanSimbol_i = document.createElement("span");
+    spanSimbol_i.textContent = "  X  ";
+    const spanFactor2_i = document.createElement("span");
     spanFactor2_i.className = "num-plomo";
-    spanFactor2_i.textContent = i
-    const spanSimbolResult_i = document.createElement("span")
+    spanFactor2_i.textContent = i;
+    const spanSimbolResult_i = document.createElement("span");
     spanSimbolResult_i.className = "signo";
     spanSimbolResult_i.textContent = "=";
-    const spanResult_i = document.createElement("span")
+    const spanResult_i = document.createElement("span");
     spanResult_i.className = "resultado";
-    spanResult_i.textContent = 5 * i
-  
-  
-    spanOperation_i.appendChild(spanFactor1_i)
-    spanOperation_i.appendChild(spanSimbol_i)
-    spanOperation_i.appendChild(spanFactor2_i)
-  
-  
-    file_i.appendChild(spanOperation_i)
-    file_i.appendChild(spanSimbolResult_i)
-    file_i.appendChild(spanResult_i)
-    table.appendChild(file_i)
+    spanResult_i.textContent = numberValue * i;
 
-    
+    spanOperation_i.appendChild(spanFactor1_i);
+    spanOperation_i.appendChild(spanSimbol_i);
+    spanOperation_i.appendChild(spanFactor2_i);
+
+    file_i.appendChild(spanOperation_i);
+    file_i.appendChild(spanSimbolResult_i);
+    file_i.appendChild(spanResult_i);
+    table.appendChild(file_i);
   }
-
-  
-
-
 };
